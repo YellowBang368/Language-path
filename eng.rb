def62f1c685872eea96b67bee361cc9a95190e9a
Антон Азров < >
2016-11-14 20:00:19 +0300

    Alpha 2.0

--- eng.rb
+++ eng.rb
@@ -12,17 +12,20 @@ require 'tkextlib/tile'
 ########################
 ########################
 def gg
-	gg_page = TkRoot.new do
-		title "Language-path(eng)"
-		minsize(400,400)
-	end	
-	gg_page_again = TkButton.new(gg_page) do
+	$frame_start.pack_forget
+	$frame_gg = TkFrame.new($main_menu) do
+		background "red"
+		padx 15
+		pady 20
+		pack('side' => 'left')
+	end
+	gg_page_again = TkButton.new($frame_gg) do
 		text "Again"
 		pack("side"=>"right", "padx"=> "50", "pady"=> "10")
 		pack("side"=>"right", "padx"=> "50", "pady"=> "10")
 		command (proc {start})
 	end
-	gg_page_exit = TkButton.new(gg_page) do
+	gg_page_exit = TkButton.new($frame_gg) do
 		text "exit"
 		pack("side"=>"right", "padx"=> "50", "pady"=> "10")
 		command(proc {exit})
@@ -42,19 +45,29 @@ end
 $keys = $eng_hash.each_key.to_a # Создаем массив из КЛЮЧЕЙ хеша $eng_hash
 $values = $eng_hash.each_value.to_a # Создаем массив из ЗНАЧЕНИЙ хеша $eng_hash
 
-main_menu = TkRoot.new do
+$main_menu = TkRoot.new do
 	title "Language-path(eng)"
 	minsize(400,400)
 end
+$frame_start = TkFrame.new($main_menu) {}
 
-start_button = TkButton.new(main_menu) do
+$start_button = TkButton.new($main_menu) do
 	text "start"
 	command(proc {start})
 	pack("side"=> "left", "padx"=> "30", "pady"=> "5")
 end
 
+menu = TkMenu.new($main_menu) # создаём меню
+menu.add('command', 'label' => "English") #, 'command' => eng_click
+menu.add('command', 'label' => "Español")
+menu.add('command', 'label' => "Deutsche")
+bar = TkMenu.new
+bar.add('cascade', 'menu' => menu, 'label' => "Languages")
+$main_menu.menu(bar)
 
 def start
+	$start_button.pack_forget
+	$frame_start.pack_forget
 	key = $keys.sample # Рандомный item из массива $keys
 	# (ОЧЕНЬ ВАЖНО) Переменая key является тем самым словом, перевод которого бы будем выбирать в приложении
 	true_value = $eng_hash[key] # Правильный перевод
@@ -96,25 +109,25 @@ def start
 	if v3 == true_value
 		puts "v3"
 	end
-	########################
-	########################
-	########################
-	eng_page = TkRoot.new do
-		title "Language-path(eng)"
-		minsize(400,400)
+
+	$frame_start = TkFrame.new($main_menu) do
+		background "red"
+		padx 15
+		pady 20
+		pack('side' => 'left')
 	end
 
-	eng_page_lbl = TkLabel.new(eng_page) do  
+	$eng_page_lbl = TkLabel.new($frame_start) do  
 		text "English"
 	 	pack("padx"=> "0", "pady"=> "10")
 	end
 
-	eng_page_key = TkButton.new(eng_page) do
+	$eng_page_key = TkButton.new($frame_start) do
 		text key
 		pack('padx'=>0	,'pady'=>30)
 	end
 
-	value1 = TkButton.new(eng_page) do
+	$value1 = TkButton.new($frame_start) do
 		text v1
 		if v1 == true_value
 			command (proc {start})
@@ -125,7 +138,7 @@ def start
 	  	pack("side"=> "left", "padx"=> "30", "pady"=> "5")
 	end
 
-	value2 = TkButton.new(eng_page) do
+	$value2 = TkButton.new($frame_start) do
 		text v2
 		if v2 == true_value
 			command (proc {start})
@@ -136,7 +149,7 @@ def start
 	  	pack("side"=> "left", "padx"=> "30", "pady"=> "5")
 	end
 
-	value3 = TkButton.new(eng_page) do
+	$value3 = TkButton.new($frame_start) do
 		text v3
 		if v3 == true_value
 			command (proc {start})
@@ -146,6 +159,9 @@ def start
 		end
 	  	pack("side"=> "left", "padx"=> "30", "pady"=> "5")
 	end
+	
 end
+
+
 
 Tk.mainloop
